05e8d95f2f38f151fd0e949064bde16ebe160873
Jacob Hardin <jhardin380@gmail.com>
2024-01-31 16:00:22 -0700

    committing notes.

---
+++ Notes/data_and_types.py
@@ -0,0 +1,23 @@
+## Jacob Hardin
+## Numbers, strings, etc
+## CSCI 110 - Python
+
+## Type functions determine what the class or type of a thing is.
+print("Program Beginning")
+print("Another line")
+print(type(100))
+print('"100.0"')
+
+
+## Variables
+variableName = "100"
+print(type(variableName))
+variableName = int(variableName)
+print(type(variableName))
+
+variableNameTwo = 3.74
+print(type(variableNameTwo))
+print(variableNameTwo*3.12)
+## variableNameTwo = int(variableNameTwo)
+print(variableNameTwo)
+
