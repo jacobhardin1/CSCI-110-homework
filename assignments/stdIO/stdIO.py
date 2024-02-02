7c231f3d50cc9aaf8ab8489a54d27794cbf4c146
Jacob Hardin <jhardin380@gmail.com>
2024-02-02 14:23:15 -0700

    updating main python script.

--- assignments/stdIO/stdIO.py
+++ assignments/stdIO/stdIO.py
@@ -1,7 +1,53 @@
+## Jacob Hardin
+## Assignment - Standard Input and Output
+
+cardOne = r'''---------
+|A      |
+| /^\/^\|
+| \    /|
+|  \  / |
+|   \/  |
+|      A|
+---------'''
+cardTwo = r'''---------
+|2      |
+|   /\  |
+|  /  \ |
+|  \  / |
+|   \/  |
+|      2|
+---------'''
+cardThree = r'''---------
+|K      |
+|   O   |
+|  O+O  |
+|  _&_  |
+|       |
+|      K|
+---------'''
+cardFour = r'''---------
+|7      |
+|   /\  |
+|  /  \ |
+| (____)|
+|   ||  |
+|      7|
+---------'''
 name = input("Please enter your name: ")
 name = name + "."
 print("Hello, ", name, "Here's a few playing cards. Don't show anyone else your hand!")
+print(cardOne)
+print(cardTwo)
+print(cardThree)
+print(cardFour)
 
-f= open(r'assignments\stdIO\ascii_art.txt','r')
+## This section calls the text file ascii_art.txt and prints the same
+## cards seen above (cardOne through cardFour).
+## I'm keeping it in as a comment, as it was just a first attempt to
+## print out ascii, without cluttering up the top area with a set of
+## multi-line variables.
 
-print(''.join([line for line in f]))
+
+## f= open(r'assignments\stdIO\ascii_art.txt','r')
+
+## print(''.join([line for line in f]))
