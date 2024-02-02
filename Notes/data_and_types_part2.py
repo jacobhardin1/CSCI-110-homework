fe636babd8114988f666e8ba18f4c84eeb6de7bb
Jacob Hardin <jhardin380@gmail.com>
2024-02-02 16:49:09 -0700

    new notes file

---
+++ Notes/data_and_types_part2.py
@@ -0,0 +1,28 @@
+## This is a continuation of the file named "data_and_types.py".
+
+## Bitwise
+
+## There are several number system bases.
+## binary (base 2, 0 and 1)
+## Octal (base 8, 0 through 7. Think counting w/ fingers, except thumbs missing)
+## Decimal (base 10, 0 through 9. Think counting w/ fingers)
+## Hexadecimal (Base 16, 0 through 9 + a through f)
+## base 360 (circles! We don't use this except in circle math)
+
+print("This is the hexadecimal form of the decimal number 255: ", hex(255))
+print("This is the binary form of the decimal number 255: ", bin(255))
+print("This is the binary form of the hex number FD: ", bin(0xFD))
+print("This is the dec number of the hex number FD: ", int(0xFD))
+## Negative numbers
+
+## Two's complement - invert all bits. Add one.
+
+## bitwise left shift: << - multiply bin number by 2 (imagine multiplying by 10)
+## bitwise right shift: >> - divide bin num by 2 (imagine div by 10)
+## bitwise AND - &
+## bitwise OR - |
+## bitwise NOT - ~
+## bitwie XOR - ^
+
+## format(value, format_spec) = formats the given value using the format.
+
