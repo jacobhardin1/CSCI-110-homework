66bb3ccb8617eb9e4918f191eed984661241c7a6
Jacob Hardin <jhardin380@gmail.com>
2024-02-02 13:37:02 -0700

    more work to push

--- assignments/stdIO/stdIO.py
+++ assignments/stdIO/stdIO.py
@@ -2,8 +2,6 @@ name = input("Please enter your name: ")
 name = name + "."
 print("Hello, ", name, "Here's a few playing cards. Don't show anyone else your hand!")
 
-def print_ascii(fn):
-    f= open(fn, 'r')
-    print(' '.join([line for line in f]))
+f= open(r'assignments\stdIO\ascii_art.txt','r')
 
-print_ascii('ascii_art.txt')
+print(''.join([line for line in f]))
