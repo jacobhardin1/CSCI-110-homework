c9c20f98340b594ad6540828a612105d9725992f
Jacob Hardin <jhardin380@gmail.com>
2024-02-07 16:32:32 -0700

    updates + adding new notes file.

---
+++ Notes/data_and_types_part3.py
@@ -0,0 +1,14 @@
+## This is a continuation of data_and_types.py and data_and_types_part2.py.
+## Please refer back to those for examples of things.
+
+## max(iterable, ...) or max(num1, num2, num3, ...)
+## separators and end 
+
+print("hello world", "text file", "goodgrnjsf", sep= '')
+
+num1 = 32.5
+num2 = 30183
+
+print("max = ", max(num1, num2))
+print("min = ", min(num1, num2))
+
--- Notes/kattis_problem.py
+++
@@ -1,5 +0,0 @@
-a = "100"
-operand = "*"
-b = "100"
-expression = a + operand + b
-print(eval(expression))
