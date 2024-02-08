a1c95fa29559b868a917f07ffc0e76bf505d9fd5
Jacob Hardin <jhardin380@gmail.com>
2024-02-08 16:39:46 -0700

    Triangle program, using functions.

---
+++ tried angles/trying_angles_with_functions.py
@@ -0,0 +1,42 @@
+
+
+
+import math
+
+def calculate_area(a, b, c):
+    s = (a + b + c) / 2
+    area = math.sqrt(s * (s - a) * (s - b) * (s - c))
+    return area
+
+def calculate_perimeter(a, b, c):
+    return a + b + c
+
+def calculate_angles(a, b, c):
+    angle_a = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
+    angle_b = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
+    angle_c = 180 - angle_a - angle_b
+    return angle_a, angle_b, angle_c
+
+def is_valid_triangle(a, b, c):
+    return a + b > c and a + c > b and b + c > a
+
+def main():
+    a = float(input("Enter the length of side a: "))
+    b = float(input("Enter the length of side b: "))
+    c = float(input("Enter the length of side c: "))
+
+    if is_valid_triangle(a, b, c):
+        area = calculate_area(a, b, c)
+        perimeter = calculate_perimeter(a, b, c)
+        angles = calculate_angles(a, b, c)
+        print("Area of the triangle:", area)
+        print("Perimeter of the triangle:", perimeter)
+        print("Angles of the triangle (in degrees):")
+        print("Angle A:", angles[0])
+        print("Angle B:", angles[1])
+        print("Angle C:", angles[2])
+    else:
+        print("The given side lengths do not form a valid triangle.")
+
+if __name__ == "__main__":
+    main()
