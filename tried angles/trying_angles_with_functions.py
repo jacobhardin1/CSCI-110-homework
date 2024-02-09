a7f2fb25b0cc159d2cf52493a74cffd0f0564ed1
Jacob Hardin <jhardin380@gmail.com>
2024-02-08 17:37:44 -0700

    comments

--- tried angles/trying_angles_with_functions.py
+++ tried angles/trying_angles_with_functions.py
@@ -10,28 +10,35 @@
 
 import math
 
+## Area function
 def calculate_area(a, b, c):
     s = (a + b + c) / 2
     area = math.sqrt(s * (s - a) * (s - b) * (s - c))
     return area
 
+## Perimeter function
 def calculate_perimeter(a, b, c):
     return a + b + c
 
+## Angles function
 def calculate_angles(a, b, c):
     angle_a = math.degrees(math.acos((b**2 + c**2 - a**2) / (2 * b * c)))
     angle_b = math.degrees(math.acos((a**2 + c**2 - b**2) / (2 * a * c)))
     angle_c = 180 - angle_a - angle_b
     return angle_a, angle_b, angle_c
 
+## Validity of the triangle
 def is_valid_triangle(a, b, c):
     return a + b > c and a + c > b and b + c > a
 
+## Asking for inputs - floats!
 def main():
     a = float(input("Enter the length of side a: "))
     b = float(input("Enter the length of side b: "))
     c = float(input("Enter the length of side c: "))
 
+## Start of program - starts with validity, then does area, perimeter
+## and angles. Prints everything on separate lines.
     if is_valid_triangle(a, b, c):
         area = calculate_area(a, b, c)
         perimeter = calculate_perimeter(a, b, c)
