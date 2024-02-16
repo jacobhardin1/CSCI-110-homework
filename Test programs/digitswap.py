63414e683b8c02e286028a56583908a51e28908c
Jacob Hardin <jhardin380@gmail.com>
2024-02-16 16:21:50 -0700

    new katis problem

---
+++ Test programs/digitswap.py
@@ -0,0 +1,19 @@
+'''
+Written by: Jacob Hardin
+Date: 02/16/2024
+Program description: 
+    https://open.kattis.com/problems/digitswap
+    
+Algorithm steps:
+    1. input a number, which will be 2 digits
+    2. make a variable for each digit
+    3. print them in opposite order'''
+
+import sys
+
+## print("Enter a number: ", file=sys.stderr) # Kattis ignores this
+
+number = input()
+
+num1 = number[0]
+num2 = number[1]
